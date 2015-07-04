ca046d06d793986f5a4821be749b924ddf24490c
Luoxi Pan <l.panpax@gmail.com>
2015-07-04 17:29:13 +1000

    added CSV reading

--- js/app.js
+++ js/app.js
@@ -11,9 +11,31 @@ app.directive('quiz', function(quizFactory) {
                 scope.quizOver = false;
                 scope.inProgress = true;
                 scope.getQuestion();
+                scope.fields = ["TITLE", "CAMPUS", "DEPARTMENT", "SCHOOL", "CAREERS", "PROGRAM_OUTLINE"];
 
                 scope.characterTags = [];
                 scope.charAttributes = {'strength': 0};
+
+                scope.JSON = {};
+
+
+                /* Fed Uni CSV Fields
+                 * ------------------
+                 * TITLE
+                 * CAMPUS
+                 * FACULTY
+                 * SCHOOL
+                 * CAREERS
+                 *
+                 * */
+                d3.csv("data/PROGRAM_DATA_BACHELOR.csv", function(data) {
+                    scope.JSON.degree = data[0].TITLE;
+                    scope.JSON.faculty = data[0].FACULTY;
+                    scope.JSON.career = ((data[0].CAREER_OPPORTUNITIES != "") ? data[0].CAREER_OPPORTUNITIES : "N/A");
+                    scope.JSON.description = data[0].PROGRAM_OUTLINE;
+
+                    console.log(scope.degreeJSON);
+                });
 
             };
 
@@ -68,7 +90,7 @@ app.factory('quizFactory', function() {
         {
             question: "Are you an indoor or outdoor person?",
             options: ["Indoor", "Outdoor"]
-        },
+        } /*,
         {
             question: "What was your favourite subject in school?",
             options: ["Maths", "Biology", "English", "Sports"]
@@ -76,7 +98,7 @@ app.factory('quizFactory', function() {
         {
             question: "Are you...",
             options: ["Creative", "Logical", "Business-oriented", "Hands on"]
-        }
+        } */
     ];
 
     return {
